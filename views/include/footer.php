a0f6d4d66d149a11ec2621e859fbce735e4a3e35
francoChavez20 <francochavez2121@gmail.com>
2024-11-07 08:12:10 -0500

    Update footer.php

--- views/include/footer.php
+++ views/include/footer.php
@@ -43,4 +43,5 @@
     </div>
   </body>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-</html>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  </html>

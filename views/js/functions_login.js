11069e01137e05c810dc45150632143a8b7f012b
francoChavez20 <francochavez2121@gmail.com>
2024-12-05 08:21:57 -0500

    SII

--- views/js/functions_login.js
+++ views/js/functions_login.js
@@ -18,7 +18,7 @@ async function iniciar_sesion() {
         json = await respuesta.json();
         if (json.status) {
             //swal("Iniciar Sesion",json.mensaje,"succes");
-            location.replace(base_url+'inicio');
+            location.replace(base_url+'/views/inicio');
         }else{
             swal("Iniciar Sesion",json.mensaje,"error");
         }
